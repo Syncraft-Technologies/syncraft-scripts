06ccb943b47b9c8237451babe8ea9f6bab0f0ad8
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-01-15 09:19:31 -0300

    fix: boot video path

--- scripts/boot_video.py
+++ scripts/boot_video.py
@@ -5,4 +5,4 @@ def play_video(path: str):
 	return subprocess.run(["sudo", "-u", "pi", "cvlc", "-f", "--no-video-title-show", path])
 
 def play_boot_video():
-	play_video(os.path.join("boot_videos", "default.mp4"))
+	play_video(os.path.join(os.path.expanduser("~"), "syncraft-scripts", "boot_videos", "default.mp4"))
