134490e1eb1dc41dd5caedf9ab8acc6710c20153
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-01-17 09:58:56 -0300

    Use symlink instead of copying rc.local

--- build/apply.sh
+++ build/apply.sh
@@ -1,4 +1,4 @@
 chmod +x /home/pi/syncraft-scripts/scripts/main.sh
 
 chmod +x /home/pi/syncraft-scripts/build/rc.local
-cat /home/pi/syncraft-scripts/build/rc.local > /etc/rc.local
+ln -s /home/pi/syncraft-scripts/build/rc.local /etc/rc.local
