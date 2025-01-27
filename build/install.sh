a406f04fee2b5ae974b7b32674a9bafe1deb2a1f
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-01-27 10:33:02 -0300

    Implement installing service on install script

--- build/install.sh
+++ build/install.sh
@@ -1,2 +1,11 @@
-cat /home/pi/syncraft-scripts/build/rc.local > /etc/rc.local
+SCRIPTPATH=$(dirname -- "$(readlink -f -- "$0")")
+
+cat "$SCRIPTPATH"/build/rc.local > /etc/rc.local
 chmod +x /etc/rc.local
+
+cat "$SCRIPTPATH"/build/syncraft-scripts.service > /etc/systemd/system/syncraft-scripts.service
+sudo systemctl unmask syncraft-scripts.service
+sudo systemctl daemon-reload
+sudo systemctl enable syncraft-scripts
+sudo systemctl set-default multi-user.target
+sudo adduser "$USER" tty
