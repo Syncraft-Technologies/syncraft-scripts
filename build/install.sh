7cebdd4183ec3741d68a038fd7403005d880ff6c
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-01-27 14:51:43 -0300

    Remove rc.local

--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,5 @@
 SCRIPTPATH=$(dirname -- "$(readlink -f -- "$0")")
 
-cat "$SCRIPTPATH"/rc.local > /etc/rc.local
-chmod +x /etc/rc.local
-
 cat "$SCRIPTPATH"/syncraft-scripts.service > /etc/systemd/system/syncraft-scripts.service
 systemctl unmask syncraft-scripts.service
 systemctl daemon-reload
