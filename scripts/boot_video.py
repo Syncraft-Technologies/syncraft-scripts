4a9512b87479752eb7c3e354c0e7e44670925408
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-02-12 10:24:33 -0300

    Run omxplayer instead of cvlc on buster machines

--- scripts/boot_video.py
+++ scripts/boot_video.py
@@ -1,8 +1,22 @@
 import subprocess
 from constants import *
 
+def get_os_codename():
+	try:
+		with open("/etc/os-release") as f:
+			for line in f:
+				if line.startswith("VERSION_CODENAME="):
+					return line.strip().split("=")[1]
+	except FileNotFoundError:
+			return None
+
 def play_video(path: str):
-	return subprocess.run(["cvlc", "-f", "--no-video-title-show", path])
+	os_codename = get_os_codename()
+
+	if os_codename == "buster":
+		subprocess.run(["omxplayer", path])
+	elif os_codename == "bullseye":
+		subprocess.run(["cvlc", "-f", "--no-video-title-show", path])
 
 def play_boot_video():
 	play_video(BOOT_VIDEO_PATH)
