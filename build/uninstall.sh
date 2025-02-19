408a7436160fc43fb4e9ef6e67e7efb68aee337d
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-02-19 15:43:58 -0300

    feat: add syncraft-usb at uninstall script

--- build/uninstall.sh
+++ build/uninstall.sh
@@ -1,2 +1,10 @@
 sudo service syncraft-scripts stop
 sudo rm /etc/systemd/system/syncraft-scripts.service
+
+apt-get remove udiskie
+sudo rm /etc/udev/rules.d/99-udisks2.rules
+sudo rm /etc/polkit-1/localauthority/50-local.d/10-udisks.pkla
+rm -rf ~/printer_data/gcodes/USB
+rm -rf ~/printer_data/gcodes/.JOB
+service syncraft-usb stop
+rm /etc/systemd/system/syncraft-usb.service
